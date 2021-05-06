4a61a3aa716ac1dbfff807fb1c43aebe4a96660f
tttuan2703 <78038011+tttuan2703@users.noreply.github.com>
2021-05-06 13:41:11 +0700

    ttt
    TTT

---
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return PartialView();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return PartialView();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return PartialView();
+        }
+        public ActionResult OnGetPartial()
+        {
+            Thread.Sleep(2000);
+            return new PartialViewResult
+            {
+                ViewName = "_HelloWorldPartial",
+                ViewData = this.ViewData
+            };
+        }
+    }
+}
